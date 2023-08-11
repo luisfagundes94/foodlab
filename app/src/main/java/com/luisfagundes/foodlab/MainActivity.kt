31bccfaf2983bcaabd0f922a541a6daa000ef017
luisf <luisfelipecf94@gmail.com>
2023-08-11 20:04:26 -0300

    configuring main activity

--- app/src/main/java/com/luisfagundes/foodlab/MainActivity.kt
+++ app/src/main/java/com/luisfagundes/foodlab/MainActivity.kt
@@ -2,44 +2,104 @@ package com.luisfagundes.foodlab
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.luisfagundes.domain.enums.DarkThemeConfig
+import com.luisfagundes.foodlab.presentation.MainUiState
+import com.luisfagundes.foodlab.presentation.MainViewModel
+import com.luisfagundes.foodlab.ui.FoodlabApp
+import com.luisfagundes.remote.utils.NetworkMonitor
 import com.luisfagundes.resources.theme.FoodlabTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+private val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+private val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var uiState: MainUiState by mutableStateOf(MainUiState.Loading)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.onEach { uiState = it }.collect()
+            }
+        }
+
+        enableEdgeToEdge()
+
         setContent {
-            FoodlabTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            val darkTheme = shouldUseDarkTheme(uiState)
+
+            DisposableEffect(darkTheme) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { darkTheme },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        lightScrim,
+                        darkScrim,
+                    ) { darkTheme },
+                )
+                onDispose {}
+            }
+
+            FoodlabTheme(
+                darkTheme = darkTheme,
+                dynamicColor = shouldDisableDynamicTheming(uiState = uiState)
+            ) {
+                FoodlabApp(
+                    windowSizeClass = calculateWindowSizeClass(activity = this),
+                    networkMonitor = networkMonitor
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun shouldDisableDynamicTheming(
+    uiState: MainUiState,
+): Boolean = when (uiState) {
+    MainUiState.Loading -> false
+    is MainUiState.Success -> !uiState.userData.useDynamicColor
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FoodlabTheme {
-        Greeting("Android")
+private fun shouldUseDarkTheme(
+    uiState: MainUiState,
+): Boolean = when (uiState) {
+    MainUiState.Loading -> isSystemInDarkTheme()
+    is MainUiState.Success -> when (uiState.userData.darkThemeConfig) {
+        DarkThemeConfig.FOLLOW_SYSTEM -> isSystemInDarkTheme()
+        DarkThemeConfig.LIGHT -> false
+        DarkThemeConfig.DARK -> true
     }
 }
