8d53547b294601552c25e4f628feb45f5ebd36c1
luisf <luisfelipecf94@gmail.com>
2023-08-13 20:52:54 -0300

    refactoring base classes

--- framework/src/main/java/com/luisfagundes/framework/base/BaseUiState.kt
+++ framework/src/main/java/com/luisfagundes/framework/base/BaseUiState.kt
@@ -5,11 +5,12 @@ import com.luisfagundes.framework.network.Result
 sealed interface BaseUiState<out R> {
     data object Loading : BaseUiState<Nothing>
     data class Success<T>(val data: T) : BaseUiState<T>
-    data class Error<T>(val throwable: Throwable) : BaseUiState<T>
+    data object Error : BaseUiState<Nothing>
 }
 
 fun <I, O> Result<I>.mapToUiState(onSuccess: (I) -> BaseUiState.Success<O>): BaseUiState<O> =
     when (this) {
-        is com.luisfagundes.framework.network.DataState.Result.Success -> onSuccess(data)
-        is com.luisfagundes.framework.network.DataState.Result.Error -> BaseUiState.Error(error)
+        is Result.Success -> onSuccess(data)
+        is Result.Error -> BaseUiState.Error
+        is Result.Loading -> BaseUiState.Loading
     }
--- framework/src/main/java/com/luisfagundes/framework/base/mvvm/MvvmViewModel.kt
+++ framework/src/main/java/com/luisfagundes/framework/base/mvvm/MvvmViewModel.kt
@@ -15,41 +15,10 @@ abstract class MvvmViewModel : ViewModel() {
 
     private val handler = CoroutineExceptionHandler { _, exception ->
         Timber.tag(SAFE_LAUNCH_EXCEPTION).e(exception)
-        handleError(exception)
     }
-
-    open fun handleError(exception: Throwable) {}
-
-    open fun startLoading() {}
 
     protected fun safeLaunch(block: suspend CoroutineScope.() -> Unit) {
         viewModelScope.launch(handler, block = block)
-    }
-
-    protected suspend fun <T> call(
-        callFlow: Flow<T>,
-        completionHandler: (collect: T) -> Unit = {}
-    ) {
-        callFlow
-            .catch { handleError(it) }
-            .collect {
-                completionHandler.invoke(it)
-            }
-    }
-
-    protected suspend fun <T> execute(
-        callFlow: Flow<Result<T>>,
-        completionHandler: (collect: T) -> Unit = {}
-    ) {
-        callFlow
-            .onStart { startLoading() }
-            .catch { handleError(it) }
-            .collect { state ->
-                when (state) {
-                    is com.luisfagundes.framework.network.DataState.Result.Error -> handleError(state.error)
-                    is com.luisfagundes.framework.network.DataState.Result.Success -> completionHandler.invoke(state.data)
-                }
-            }
     }
 
     companion object {
