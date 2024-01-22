749703ec06335226688c2cda688b32f69f02ddab
lfMint <luisfelipecf94@gmail.com>
2024-01-22 12:35:42 -0300

    renaming file

--- data/src/main/java/com/luisfagundes/data/local/di/RepositoryModule.kt
+++ data/src/main/java/com/luisfagundes/data/local/di/RepositoryModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface RemoteDataModule {
+interface RepositoryModule {
     @Binds
     fun bindsUserDataRepository(
         userDataRepository: UserDataRepositoryImpl,
